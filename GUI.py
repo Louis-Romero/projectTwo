0c82eee38ce32286a00d9f3ea740ef63a39a8ed9
Chris <cforsythe@csumb.edu>
2016-03-16 15:33:52 -0700

    FileDialog change

--- GUI.py
+++ GUI.py
@@ -1,4 +1,6 @@
 import Tkinter as tk   # python
+from Tkinter import *
+import tkFileDialog
 
 TITLE_FONT = ("Helvetica", 18, "bold")
 
@@ -12,13 +14,13 @@ class Text2PNG(tk.Tk):
         # will be raised above the others
         container = tk.Frame(self)
         container.pack(side="top")
-        self.geometry("418x405")
+        self.geometry("415x405")
         self.title("Text2PNG")
         #container.grid_rowconfigure(0, weight=1);container.grid_rowconfigure(1, weight=1);
         container.grid_columnconfigure(0, weight=1);container.grid_columnconfigure(1, weight=1);
         container.grid_columnconfigure(2, weight=1);
         self.frames = {}
-        for F in (mainframe, picframe, textframe, submitframe):
+        for F in (picframe, textframe, submitframe):
             page_name = F.__name__
             frame = F(container, self)
             self.frames[page_name] = frame
@@ -28,33 +30,12 @@ class Text2PNG(tk.Tk):
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("mainframe")
+        self.show_frame("picframe")
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
-
-
-class mainframe(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-
-        submitpage = tk.Button(self, text="Hide text", height=2, width = 16,
-                            command=lambda: controller.show_frame("submitframe"))
-        imagepage = tk.Button(self, text="Choose an image", height=2, width=16,
-                            command=lambda: controller.show_frame("picframe"))
-        textpage = tk.Button(self, text="Choose a text file",height =2, width=16,
-                            command=lambda: controller.show_frame("textframe"))
-        browseimage = tk.Button(self, text="Browse Images", height = 8, width = 51)
-        showimage = tk.Button(self, text="Show Image", height = 8, width = 51)
-        submitpage.grid(row=0, column=0)
-        imagepage.grid(row=0, column=1);
-        textpage.grid(row=0, column =2)
-        browseimage.grid(row=1, column =0, columnspan = 3)
-        showimage.grid(row=2, column =0, columnspan = 3)
 
 
 
@@ -68,10 +49,10 @@ class submitframe(tk.Frame):
                             command=lambda: controller.show_frame("picframe"))
         textpage = tk.Button(self, text="Choose a text file",height =2, width=16,
                             command=lambda: controller.show_frame("textframe"))
-        startencr = tk.Button(self, text="Start Hiding Process\n (will show picture when finished)", height = 8, width = 51)
-        saveimage = tk.Button(self, text="Save Encrypted Image", height = 8, width = 51)
-        submitpage.grid(row=0, column=0);imagepage.grid(row=0, column=1);textpage.grid(row=0, column =2);
-        startencr.grid(row=1, column =0, columnspan = 3);saveimage.grid(row=2, column =0, columnspan = 3)
+        startencr = tk.Button(self, text="Start Hiding Process\n (will show picture when finished)", height = 8)
+        saveimage = tk.Button(self, text="Save Encrypted Image", height = 8)
+        submitpage.grid(row=0, column=2);imagepage.grid(row=0, column=0);textpage.grid(row=0, column =1);
+        startencr.grid(row=1, column =0, columnspan = 3, sticky=N+E+W+S);saveimage.grid(row=2, column =0, columnspan = 3, sticky=N+E+W+S)
 
 
 class picframe(tk.Frame):
@@ -85,13 +66,19 @@ class picframe(tk.Frame):
                             command=lambda: controller.show_frame("picframe"))
         textpage = tk.Button(self, text="Choose a text file",height =2, width=16,
                             command=lambda: controller.show_frame("textframe"))
-        browseimage = tk.Button(self, text="Browse Images", height = 8, width = 51)
-        showimage = tk.Button(self, text="Show Image", height = 8, width = 51)
-        submitpage.grid(row=0, column=0)
-        imagepage.grid(row=0, column=1);
-        textpage.grid(row=0, column =2)
-        browseimage.grid(row=1, column =0, columnspan = 3)
-        showimage.grid(row=2, column =0, columnspan = 3)
+        browseimage = tk.Button(self, text="Browse Images", height = 8, command=self.onOpen)
+        showimage = tk.Button(self, text="Show Image", height = 8)
+        submitpage.grid(row=0, column=2)
+        imagepage.grid(row=0, column=0);
+        textpage.grid(row=0, column =1)
+        browseimage.grid(row=1, column =0, columnspan = 3, sticky=W+E)
+        showimage.grid(row=2, column =0, columnspan = 3, sticky=W+E)
+    def onOpen(self):
+      
+        ftypes = [('PNG Files', '*.png')]
+        pngimage = tkFileDialog.askopenfilename(filetypes = ftypes)
+        
+
         
 class textframe(tk.Frame):
 
@@ -104,13 +91,13 @@ class textframe(tk.Frame):
                             command=lambda: controller.show_frame("picframe"))
         textpage = tk.Button(self, text="Choose a text file",height =2, width=16,
                             command=lambda: controller.show_frame("textframe"))
-        browseimage = tk.Button(self, text="Browse Text Files", height = 8, width = 51)
-        showimage = tk.Button(self, text="Show Text File", height = 8, width = 51)
-        submitpage.grid(row=0, column=0)
-        imagepage.grid(row=0, column=1);
-        textpage.grid(row=0, column =2)
-        browseimage.grid(row=1, column =0, columnspan = 3)
-        showimage.grid(row=2, column =0, columnspan = 3)
+        browseimage = tk.Button(self, text="Browse Text Files", height = 8)
+        showimage = tk.Button(self, text="Show Text File", height = 8)
+        submitpage.grid(row=0, column=2)
+        imagepage.grid(row=0, column=0);
+        textpage.grid(row=0, column =1)
+        browseimage.grid(row=1, column =0, columnspan = 3, sticky=W+E)
+        showimage.grid(row=2, column =0, columnspan = 3, sticky=W+E)
 
 
 if __name__ == "__main__":
