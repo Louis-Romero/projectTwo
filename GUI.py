3b0772a0bb796643b3a9226cb59720189f664acb
Chris <cforsythe@csumb.edu>
2016-03-30 16:03:46 -0700

    Some serious changes tell me if it works

--- GUI.py
+++ GUI.py
@@ -4,9 +4,18 @@ import tkFileDialog
 from PIL import Image
 import os
 import webbrowser
+import functions
+import hideTextInImage
+import unhideTextInImage
 
 TITLE_FONT = ("Helvetica", 18, "bold")
-#pathvar = ""
+pathvar = ""
+textpathvar = ""
+encimage = ""
+
+
+
+
 class Text2PNG(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -17,13 +26,11 @@ class Text2PNG(tk.Tk):
         # will be raised above the others
         container = tk.Frame(self)
         container.pack(side="top")
-        self.geometry("415x405")
         self.title("Text2PNG")
-        #container.grid_rowconfigure(0, weight=1);container.grid_rowconfigure(1, weight=1);
         container.grid_columnconfigure(0, weight=1);container.grid_columnconfigure(1, weight=1);
-        container.grid_columnconfigure(2, weight=1);
+        container.grid_columnconfigure(2, weight=1); 
         self.frames = {}
-        for F in (picframe, textframe, submitframe):
+        for F in (picframe, textframe, submitframe,decryptframe):
             page_name = F.__name__
             frame = F(container, self)
             self.frames[page_name] = frame
@@ -46,16 +53,26 @@ class submitframe(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        submitpage = tk.Button(self, text="Hide text", height=2, width = 16,  
+        submitpage = tk.Button(self, text="Hide text", height=2, width = 16, bg = "springgreen4", fg = "white",  
                             command=lambda: controller.show_frame("submitframe"))
         imagepage = tk.Button(self, text="Choose an image", height=2, width=16,
                             command=lambda: controller.show_frame("picframe"))
         textpage = tk.Button(self, text="Choose a text file",height =2, width=16,
                             command=lambda: controller.show_frame("textframe"))
-        startencr = tk.Button(self, text="Start Hiding Process\n (will show picture when finished)", height = 8)
-        saveimage = tk.Button(self, text="Save Encrypted Image", height = 8)
-        submitpage.grid(row=0, column=2);imagepage.grid(row=0, column=0);textpage.grid(row=0, column =1);
-        startencr.grid(row=1, column =0, columnspan = 3, sticky=N+E+W+S);saveimage.grid(row=2, column =0, columnspan = 3, sticky=N+E+W+S)
+        decryptpage = tk.Button(self, text = "Decrypt the Image",height =2, width=16,
+                            command=lambda: controller.show_frame("decryptframe"))
+        startencr = tk.Button(self, text="Start Hiding Process\n (will show picture when finished)", height = 8, activebackground = "turquoise2", 
+                            command=self.encryptPic)
+        saveimage = tk.Button(self, text="Save Encrypted Image", height = 8, activebackground = "turquoise2")
+        submitpage.grid(row=0, column=2);imagepage.grid(row=0, column=0);textpage.grid(row=0, column =1); decryptpage.grid(row = 0, column = 3)
+        startencr.grid(row=1, column =0, columnspan = 4, sticky=N+E+W+S);saveimage.grid(row=2, column =0, columnspan = 4, sticky=N+E+W+S)
+    def encryptPic(self):
+        global pathvar
+        global textpathvar
+        hideTextInImage.processingImage(pathvar, textpathvar)
+
+
+
 
 
 class picframe(tk.Frame):
@@ -65,29 +82,38 @@ class picframe(tk.Frame):
         self.controller = controller
         submitpage = tk.Button(self, text="Hide text", height=2, width = 16,  
                             command=lambda: controller.show_frame("submitframe"))
-        imagepage = tk.Button(self, text="Choose an image", height=2, width=16,
+        imagepage = tk.Button(self, text="Choose an image", height=2, width=16,bg = "springgreen4", fg = "white",
                             command=lambda: controller.show_frame("picframe"))
         textpage = tk.Button(self, text="Choose a text file",height =2, width=16,
                             command=lambda: controller.show_frame("textframe"))
-        browseimage = tk.Button(self, text="Browse Images", height = 8, command=self.onOpen)
-        showimage = tk.Button(self, text="Show Image", height = 8, command=self.openPic)
+        decryptpage = tk.Button(self, text = "Decrypt the Image",height =2, width=16,
+                            command=lambda: controller.show_frame("decryptframe"))
+        browseimage = tk.Button(self, text="Browse Images", height = 8, command=self.onOpen, activebackground = "turquoise2")
+        showimage = tk.Button(self, text="Show Image", height = 8, command=self.openPic, activebackground = "turquoise2")
         submitpage.grid(row=0, column=2)
         imagepage.grid(row=0, column=0);
         textpage.grid(row=0, column =1)
-        browseimage.grid(row=1, column =0, columnspan = 3, sticky=W+E)
-        showimage.grid(row=2, column =0, columnspan = 3, sticky=W+E)
+        decryptpage.grid(row = 0, column =3)
+        browseimage.grid(row=1, column =0, columnspan = 4, sticky=W+E)
+        showimage.grid(row=2, column =0, columnspan = 4, sticky=W+E)
+
     def onOpen(self):
-      
         ftypes = [('PNG Files', '*.png')]
         pngimage = tkFileDialog.askopenfilename(filetypes = ftypes)
+
         pngimage = str(pngimage)
         self.pathvar = tk.StringVar()
         self.pathvar.set(pngimage)
+        global pathvar
+        pathvar = pngimage
 
     def openPic(self):
         filepath = self.pathvar.get()
         showpng = Image.open(filepath)
         showpng.show()
+
+
+
         
 
         
@@ -100,27 +126,68 @@ class textframe(tk.Frame):
                             command=lambda: controller.show_frame("submitframe"))
         imagepage = tk.Button(self, text="Choose an image", height=2, width=16,
                             command=lambda: controller.show_frame("picframe"))
-        textpage = tk.Button(self, text="Choose a text file",height =2, width=16,
+        textpage = tk.Button(self, text="Choose a text file",height =2, width=16,bg = "springgreen4", fg = "white",
                             command=lambda: controller.show_frame("textframe"))
-        browsetext = tk.Button(self, text="Browse Text Files", height = 8, command=self.onOpen)
-        showtext = tk.Button(self, text="Show Text File", height = 8, command=self.openText)
+        decryptpage = tk.Button(self, text = "Decrypt the Image",height =2, width=16,
+                            command=lambda: controller.show_frame("decryptframe"))
+        
+
+        browsetext = tk.Button(self, text="Browse Text Files", height = 8, command=self.onOpen, activebackground = "turquoise2")
+        showtext = tk.Button(self, text="Show Text File", height = 8, command=self.openText, activebackground = "turquoise2")
         submitpage.grid(row=0, column=2)
         imagepage.grid(row=0, column=0);
         textpage.grid(row=0, column =1)
-        browsetext.grid(row=1, column =0, columnspan = 3, sticky=W+E)
-        showtext.grid(row=2, column =0, columnspan = 3, sticky=W+E)
+        decryptpage.grid(row = 0, column = 3)
+        browsetext.grid(row=1, column =0, columnspan = 4, sticky=W+E)
+        showtext.grid(row=2, column =0, columnspan = 4, sticky=W+E)
 
     def onOpen(self):
       
         ftypes = [('Text Files', '*.txt')]
         txtfile = tkFileDialog.askopenfilename(filetypes = ftypes)
         txtfile = str(txtfile)
-        self.pathvar = tk.StringVar()
-        self.pathvar.set(txtfile)
+        self.textpathvar = tk.StringVar()
+        self.textpathvar.set(txtfile)
+        global textpathvar
+        textpathvar = txtfile
 
     def openText(self):
         filepath = self.pathvar.get()
         webbrowser.open(filepath)
+
+
+class decryptframe(tk.Frame):
+    def nOpen(self):
+            ftypes = [('PNG Files', '*.png')]
+            pngimage = tkFileDialog.askopenfilename(filetypes = ftypes)
+            pngimage = str(pngimage)
+            global encimage
+            encimage = pngimage
+            
+    def __init__ (self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        submitpage = tk.Button(self, text="Hide text", height=2, width = 16,  
+                            command=lambda: controller.show_frame("submitframe"))
+        imagepage = tk.Button(self, text="Choose an image", height=2, width=16,
+                            command=lambda: controller.show_frame("picframe"))
+        textpage = tk.Button(self, text="Choose a text file",height =2, width=16,
+                            command=lambda: controller.show_frame("textframe"))
+        decryptpage = tk.Button(self, text = "Decrypt the Image",height =2, width=16, bg = "springgreen4", fg = "white", 
+                            command=lambda: controller.show_frame("decryptframe"))
+        browsedeimage = tk.Button(self, text = "Browse for the Encrypted Image", height = 8, command=self.nOpen, activebackground = "turquoise2")
+        showmess = tk.Button(self, text = "Show the Decrypted Message" , height = 8,command=self.showMessage, activebackground = "turquoise2")
+        browsedeimage.grid(row = 1, column = 0, columnspan = 4, sticky=W+E)
+        showmess.grid(row = 2, column = 0, columnspan = 4, sticky=W+E)
+        submitpage.grid(row=0, column=2)
+        imagepage.grid(row=0, column=0);
+        textpage.grid(row=0, column =1)
+        decryptpage.grid(row = 0, column = 3)
+    def showMessage(self):
+        global encimage
+        print encimage
+        unhideTextInImage.decryptImage(encimage)
+
 
 
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -5,13 +5,17 @@
 #http://www.greenteapress.com/thinkpython/thinkpython.pdf 
 
 #This function accepts an ASCII value and returns the binary string of that ASCII value
-def openFileReadText():
-	fileWithText = open('words.txt', 'r')
+from PIL import Image
+
+def openFileReadText(file):
+	fileWithText = open(file, 'r')
 	listOfWords = [] #Contains every word in text file
 	fileContent = fileWithText.read()
 	listOfWords = fileContent.split()
+	fileWithText.close()
 	return listOfWords
 
+#This function accepts a list of words and converts each word into a string of binary digit and adds a whitespace in binary after each word converted into binary
 def makeBinaryListOfWords(wordList):
 	oneWordInBinary = ""
 	wordsFromFileInBinary = []
@@ -20,15 +24,47 @@ def makeBinaryListOfWords(wordList):
 		for jx in range(0, len(word)):
 			oneWordInBinary += makeNumberBinary(ord(word[jx]))
 		wordsFromFileInBinary.append(oneWordInBinary)
-		wordsFromFileInBinary.append("00100000") # adds a whitespace
+		wordsFromFileInBinary.append("00100000")
 		oneWordInBinary = "" #clears word
 	return wordsFromFileInBinary
-	
 
 def makeNumberBinary(number):
 	binaryString = ""
 	baseTwoValues = [128, 64, 32, 16, 8, 4, 2, 1] 
 	for ix in range(0, 8):
+			if(number < baseTwoValues[ix]):
+				binaryString += "0"
+			else:
+				binaryString += "1"
+				number -= baseTwoValues[ix]
+	return binaryString
+
+#This function takes a binary string of 8-bits and converts it to it's decimal value
+def makeBinaryANumber(binaryString):
+	baseTwoValues = [128, 64, 32, 16, 8, 4, 2, 1] 
+	sumOfValues = 0
+	for ix in range(0, len(binaryString)):
+		if(binaryString[ix] == "1"):
+			sumOfValues += baseTwoValues[ix]
+		else:
+			sumOfValues += 0
+	return sumOfValues
+
+#This functions takes a binary string of 18-bits and converts it to it's decimal value
+def make18BitsANumber(binaryString):
+	baseTwoValues = [131072, 65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1] 
+	sumOfValues = 0
+	for ix in range(0, len(binaryString)):
+		if(binaryString[ix] == "1"):
+			sumOfValues += baseTwoValues[ix]
+		else:
+			sumOfValues += 0
+	return sumOfValues
+
+def makeNumber18BitBinaryString(number):
+	binaryString = ""
+	baseTwoValues = [131072, 65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1] 
+	for ix in range(0, 18):
 			if(number < baseTwoValues[ix]):
 				binaryString += "0"
 			else:
