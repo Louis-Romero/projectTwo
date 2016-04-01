af01af8a9f7b63a76237bbfb89170fdf89a77b30
Arthur Diaz (A.J) <adiaz@csumb.edu>
2016-04-01 12:38:09 -0700

    I added comments

--- GUI.py
+++ GUI.py
@@ -1,12 +1,22 @@
+
+#http://effbot.org/zone/tkinter-callbacks.htm
+#http://zetcode.com/gui/tkinter/introduction/
+#http://effbot.org/tkinterbook/tkinter-classes.htm
+
 import Tkinter as tk   # python
 from Tkinter import *
 import tkFileDialog
 from PIL import Image
 import os
 import webbrowser
+import functions
+import hideTextInImage
+import unhideTextInImage
 
 TITLE_FONT = ("Helvetica", 18, "bold")
-#pathvar = ""
+pathvar = ""
+textpathvar = ""
+encimage = ""
 
 
 
@@ -43,6 +53,8 @@ class Text2PNG(tk.Tk):
         frame.tkraise()
 
 
+#The third Tab with the hiding process options and it puts the hidden picture into
+#the folder the program is in 
 
 class submitframe(tk.Frame):
     def __init__(self, parent, controller):
@@ -56,13 +68,22 @@ class submitframe(tk.Frame):
                             command=lambda: controller.show_frame("textframe"))
         decryptpage = tk.Button(self, text = "Decrypt the Image",height =2, width=16,
                             command=lambda: controller.show_frame("decryptframe"))
-        startencr = tk.Button(self, text="Start Hiding Process\n (will show picture when finished)", height = 8, activebackground = "turquoise2")
+        startencr = tk.Button(self, text="Start Hiding Process\n (will show picture when finished)", height = 8, activebackground = "turquoise2", 
+                            command=self.encryptPic)
         saveimage = tk.Button(self, text="Save Encrypted Image", height = 8, activebackground = "turquoise2")
         submitpage.grid(row=0, column=2);imagepage.grid(row=0, column=0);textpage.grid(row=0, column =1); decryptpage.grid(row = 0, column = 3)
         startencr.grid(row=1, column =0, columnspan = 4, sticky=N+E+W+S);saveimage.grid(row=2, column =0, columnspan = 4, sticky=N+E+W+S)
 
+#Creates the pathways for the picture and text to be added into the hide text function
+    def encryptPic(self):
+        global pathvar
+        global textpathvar
+        hideTextInImage.processingImage(pathvar, textpathvar)
 
 
+
+
+#The First tab with the picture browsing options
 class picframe(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -84,23 +105,28 @@ class picframe(tk.Frame):
         decryptpage.grid(row = 0, column =3)
         browseimage.grid(row=1, column =0, columnspan = 4, sticky=W+E)
         showimage.grid(row=2, column =0, columnspan = 4, sticky=W+E)
-
+    #Where you can only browse for png files 
     def onOpen(self):
         ftypes = [('PNG Files', '*.png')]
         pngimage = tkFileDialog.askopenfilename(filetypes = ftypes)
-
+#Puts that picture into a string and creates the pathway for the function
         pngimage = str(pngimage)
         self.pathvar = tk.StringVar()
         self.pathvar.set(pngimage)
+        global pathvar
+        pathvar = pngimage
+#if you click show image it will show the image that you chose to make sure you
+#want that picture
     def openPic(self):
         filepath = self.pathvar.get()
         showpng = Image.open(filepath)
         showpng.show()
 
 
-        
 
         
+
+#Second tab with the choose text file options
 class textframe(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -124,24 +150,31 @@ class textframe(tk.Frame):
         decryptpage.grid(row = 0, column = 3)
         browsetext.grid(row=1, column =0, columnspan = 4, sticky=W+E)
         showtext.grid(row=2, column =0, columnspan = 4, sticky=W+E)
-
+#Lets you browse the computer for text files and creates a string and the pathway
+#for the function to happen    
     def onOpen(self):
       
         ftypes = [('Text Files', '*.txt')]
         txtfile = tkFileDialog.askopenfilename(filetypes = ftypes)
         txtfile = str(txtfile)
-        self.pathvar = tk.StringVar()
-        self.pathvar.set(txtfile)
-
+        self.textpathvar = tk.StringVar()
+        self.textpathvar.set(txtfile)
+        global textpathvar
+        textpathvar = txtfile
+#Will show the text to make sure that you know what text you chose
     def openText(self):
         filepath = self.pathvar.get()
         webbrowser.open(filepath)
 
-
+#The Fourth tab where you choose the file that has hidden text in the image and
+#then unhides the text and puts it in the folder that the program is in
 class decryptframe(tk.Frame):
     def nOpen(self):
             ftypes = [('PNG Files', '*.png')]
             pngimage = tkFileDialog.askopenfilename(filetypes = ftypes)
+            pngimage = str(pngimage)
+            global encimage
+            encimage = pngimage
             
     def __init__ (self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -155,25 +188,20 @@ class decryptframe(tk.Frame):
         decryptpage = tk.Button(self, text = "Decrypt the Image",height =2, width=16, bg = "springgreen4", fg = "white", 
                             command=lambda: controller.show_frame("decryptframe"))
         browsedeimage = tk.Button(self, text = "Browse for the Encrypted Image", height = 8, command=self.nOpen, activebackground = "turquoise2")
-        showmess = tk.Button(self, text = "Show the Decrypted Message" , height = 8, activebackground = "turquoise2")
+        showmess = tk.Button(self, text = "Show the Decrypted Message" , height = 8,command=self.showMessage, activebackground = "turquoise2")
         browsedeimage.grid(row = 1, column = 0, columnspan = 4, sticky=W+E)
         showmess.grid(row = 2, column = 0, columnspan = 4, sticky=W+E)
         submitpage.grid(row=0, column=2)
         imagepage.grid(row=0, column=0);
         textpage.grid(row=0, column =1)
         decryptpage.grid(row = 0, column = 3)
+    #When clicked the function of unhide text is called and it pulls the text out
+   #of the picture
+    def showMessage(self):
+        global encimage
+        print encimage
+        unhideTextInImage.decryptImage(encimage)
 
-    def onOpen(self):
-      
-        ftypes = [('Text Files', '*.txt')]
-        txtfile = tkFileDialog.askopenfilename(filetypes = ftypes)
-        txtfile = str(txtfile)
-        self.pathvar = tk.StringVar()
-        self.pathvar.set(txtfile)
-
-    def openText(self):
-        filepath = self.pathvar.get()
-        webbrowser.open(filepath)
 
 
 if __name__ == "__main__":
